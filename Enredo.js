85b1e6d967e37da046dae7bf36fd370558da9524
Alexa <jvmlopes00@gmail.com>
2022-07-26 15:38:55 -0400

    Terminei o início da parte 1 e pulei metade da história

--- Enredo.js
+++ Enredo.js
@@ -1,3 +1,7 @@
 /*
 Dio invade a vida do Jonathan e já chega chutando o cachorro dele e dispersando o ódio. Jonathan bate em Dio.
+
+Dio coloca uma máscara e mata o pai do Jonathan. Jonathan queima a casa com Dio junto. Dio fica doidão...
+
+Jonathan morre e Dio rouba o corpo dele para fazer um filho mafioso e ser morto pelo neto do Jonathan.
 */
